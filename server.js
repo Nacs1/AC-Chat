6d50e3860e2b17de4f7af8e97de3f5c6cbe6470d
Nacs1 <saddarasxd@gmail.com>
2025-09-21 17:23:53 +0300

    Fix: serve index.html at root

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Serve static files from "public"
 app.use(express.static("public"));
+
+// Serve index.html explicitly at root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected");
